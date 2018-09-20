79d195c9fac6f250afb4a6af1de7c1b4ded1426a
Tyler Thompson <th0mps0n.ty@protonmail.com>
2018-09-20 15:32:17 -0400

    created the navbar and added the various links and a button showing the user initials.

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,11 +1,15 @@
-import react from 'react';
-import { link } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import SignedInLinks from './SignedInLinks';
+import SignedOutLinks from './SignedOutLinks';
 
 const Navbar = () => {
     return (
         <nav className="nav-wrapper grey darken-3">
             <div className="container">
-                <link to='/' className="brand-logo">MarioPlan</link>
+                <Link to='/' className="brand-logo">MarioPlan</Link>
+                <SignedInLinks /> 
+                <SignedOutLinks /> 
             </div>
         </nav>
     )
